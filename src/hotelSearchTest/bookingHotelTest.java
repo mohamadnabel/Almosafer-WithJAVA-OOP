6bb7602a02947fbf01e416804c9b81b516e24920
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2024-03-21 03:14:18 +0300

    update Test search hotel searchbox

--- src/hotelSearchTest/bookingHotelTest.java
+++ src/hotelSearchTest/bookingHotelTest.java
@@ -1,6 +1,7 @@
 package hotelSearchTest;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -17,7 +18,7 @@ public class bookingHotelTest extends parameters {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 
-	@Test(invocationCount = 1, priority = 1, groups = "testNg")
+	@Test(invocationCount = 1, priority = 1, groups = "testNg", enabled = true)
 	public void myTest() {
 		WebElement hotelTap = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
 		hotelTap.click();
@@ -44,7 +45,7 @@ public class bookingHotelTest extends parameters {
 
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2, enabled = true)
 	public void randomSelectVisiterNum() {
 		WebElement selectorVisitor = driver
 				.findElement(By.xpath("//select[@data-testid='HotelSearchBox__ReservationSelect_Select']"));
@@ -54,6 +55,51 @@ public class bookingHotelTest extends parameters {
 
 		WebElement searchBtn = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
 		searchBtn.click();
+
+	}
+
+	@Test(priority = 3, enabled = true)
+	public void pageFullyCompleted() throws InterruptedException {
+		Thread.sleep(10000);
+		WebElement MsgCompleatedSearch = driver
+				.findElement(By.xpath("//span[@data-testid='HotelSearchResult__resultsFoundCount']"));
+		String MsgResulte = MsgCompleatedSearch.getText();
+		MsgResulte.contains("found");
+		System.out.println(MsgResulte);
+		Assert.assertEquals(MsgResulte.contains("found"), true);
+
+	}
+
+	@Test(priority = 4, enabled = true)
+	public void checkLowestFunctionSearch() {
+		WebElement lowestPriceBtn = driver
+				.findElement(By.cssSelector("button[data-testid='HotelSearchResult__sort__LOWEST_PRICE']"));
+		lowestPriceBtn.click();
+
+		WebElement containerOfThePrices = driver.findElement(By.cssSelector(".sc-htpNat.KtFsv.col-9"));
+
+		List<WebElement> Prices = containerOfThePrices.findElements(By.className("Price__Value"));
+
+		int firstPrice = Integer.parseInt(Prices.get(0).getText());
+
+		int lastPrice = Integer.parseInt(Prices.get(Prices.size() - 1).getText());
+
+		boolean isPriceComparisonTrue = firstPrice < lastPrice;
+//		Boolean expactedResulte = true;
+//		Assert.assertEquals(firstPrice < lastPrice, true);
+
+//		Assert.assertEquals(isPriceComparisonTrue, expactedResulte);
+
+		System.out.println(isPriceComparisonTrue + "@@@@@@@@@@");
+
+		System.out.println(firstPrice);
+		System.out.println(lastPrice);
+
+		System.out.println(date.getMonth());
+		System.out.println(date.getDayOfMonth());
+		System.out.println(date.getDayOfWeek());
+
+		System.out.print(Prices.get(5).getText() + "@@@@@@@@@@@@@@@@@@@@@@2");
 
 	}
 
--- src/hotelSearchTest/parameters.java
+++ src/hotelSearchTest/parameters.java
@@ -1,9 +1,11 @@
 package hotelSearchTest;
 
+import java.time.LocalDate;
 import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.Assertion;
 
 public class parameters {
 	WebDriver driver = new ChromeDriver();
@@ -12,5 +14,8 @@ public class parameters {
 	String[] EnglishCities = { "dubai", "jeddah", "riyadh" };
 	Random rand = new Random();
 	int randomVisitor = rand.nextInt(2);
+	Assertion Assert = new Assertion();
+	LocalDate date = LocalDate.now();
+	
 
 }
